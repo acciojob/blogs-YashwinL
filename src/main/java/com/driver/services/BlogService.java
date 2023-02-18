a7141b502c789517049ebe8dbb3f31f4fabeefe0
YashwinL <yashwinlakshmipathy@gmail.com>
2023-02-18 17:02:41 +0530

    changes

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -21,12 +21,12 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
         //create a blog at the current time
         Blog blog = new Blog();
         User user = userRepository1.findById(userId).get();
         if(!userRepository1.existsById(userId)){
-            return blog;
+            throw new Exception("user not exist");
         }
         blog.setTitle(title);
         blog.setContent(content);
